d071bad745e01472ab82d9622a45a524e33a2375
Lunf1re <154937294+Lunf1re@users.noreply.github.com>
2026-03-10 07:07:26 +0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -8,11 +8,20 @@ const mangaSearch = require("./routes/mangaSearch")
 app.use(bodyParser.json())
 require('dotenv').config()
 
+// CORS fix
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  if (req.method === 'OPTIONS') return res.sendStatus(200)
+  next()
+})
+
 app.use(ApiKey)
 app.use("/api/manga", mangaRouter)
 app.use("/api/mangaList", mangaListRouter)
 app.use("/api/search", mangaSearch)
 
 app.listen(process.env.PORT, ()=>{
-    console.log(`Server Start On Port ${process.env.PORT} 🎉✨ `)
+  console.log(`Server Start On Port ${process.env.PORT} 🚀✨ `)
 })
